ac3fc475e0d964820d1ad4b284cf1d3a93113164
CocoTheMii <44563370+CocoTheMii@users.noreply.github.com>
2024-04-30 11:29:48 -0700

    Random colours, start working on selecting tiles

--- game.py
+++ game.py
@@ -1,16 +1,25 @@
 import pygame
+import random
 
 # setup classes
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, x, y, size):
+    def __init__(self, x, y, color, size):
         super().__init__()
 
         self.image = pygame.Surface((size, size))
-        self.image.fill((255,0,0))
+        self.image.fill(color)
 
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+    def check_click(self, mouse, selected_group):
+        if self.rect.collidepoint(mouse):
+            if selected_group.has(self):
+                selected_group.remove(self)
+            else:
+                selected_group.add(self)
+            print(selected_group)
 
 # create game window
 pygame.init()
@@ -20,20 +29,28 @@ pygame.display.set_caption("Match 3 Game")
 # tile grid parameters
 start_x = 10    # starting x of grid
 start_y = 10    # starting y of grid
-size = 50       # side length of tiles
-spacing = 10    # spacing between tiles
+size = 48       # side length of tiles
+spacing = 24    # spacing between tiles
+
+colors = [pygame.Color("#F94144"),
+          pygame.Color("#F3722C"),
+          pygame.Color("#F8961E"),
+          pygame.Color("#F9C74F"),
+          pygame.Color("#90BE6D"),
+          pygame.Color("#43AA8B"),
+          pygame.Color("#577590")]
 
 # create tiles
-tiles = []
-all_sprites = pygame.sprite.Group()
+tiles = pygame.sprite.Group()
+selected = pygame.sprite.Group()
 
-for r in range(8):
-    tiles.append([])
-    for t in range(8):
-        x = start_x + (size + spacing) * (t % 8)
-        y = start_y + (size + spacing) * (r % 8) 
-        tiles[r].append(Tile(x, y, size))
-        all_sprites.add(tiles[r][t])
+for row in range(8):
+    for col in range(8):
+        x = start_x + (size + spacing) * (col % 8)
+        y = start_y + (size + spacing) * (row % 8)
+        color = random.choice(colors)
+        tiles.add(Tile(x, y, color, size))
+
 
 # game loop
 running = True
@@ -41,10 +58,17 @@ while running == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_pos = pygame.mouse.get_pos()
+            for t in tiles:
+                t.check_click(event.pos, selected)
+                t.image.fill((255,0,0))
+    for t in selected:
+        t.image.fill((0,255,0))
     
-    window.fill((255,255,255))
-    all_sprites.update()
-    all_sprites.draw(window)
+    window.fill(pygame.Color("#e9ecef"))
+    tiles.update()
+    tiles.draw(window)
     pygame.display.update()
 
 pygame.quit()
